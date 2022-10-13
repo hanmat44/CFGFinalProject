65bfe6b10f6e5b4e19b1f5c2d85290ae855cb355
hanmat44 <hannahmathews01@gmail.com>
2022-10-13 13:39:37 +0100

    Test of zoo animals API

---
+++ apitest.py
@@ -0,0 +1,10 @@
+import requests
+from pprint import pprint as pp
+
+# Test of the zoo animals API
+endpoint1 = 'https://zoo-animal-api.herokuapp.com/animals/rand'
+response = requests.get(endpoint1)
+data = response.json()
+pp(data)
+print(data['name'])
+
