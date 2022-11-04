f383b02b51adac7c754a167faf99eae09a4c80b9
hanmat44 <hannahmathews01@gmail.com>
2022-11-04 18:38:51 +0000

    Formatting

--- check_scores.py
+++ check_scores.py
@@ -1,8 +1,11 @@
 from db_utils import view_score, clear_scores
+
+
 class CheckScore:
     def __init__(self):
         self.player_score = view_score('person')
         self.computer_score = view_score('computer')
+
     def check_score(self):
         print("Your score is {}. The computer's score is {}.".format(self.player_score, self.computer_score))
 
@@ -11,7 +14,7 @@ class CheckScore:
         print("Scores reset")
 
 
-if __name__== '__main__':
+if __name__ == '__main__':
     c = CheckScore()
     c.check_score()
     c.reset_game()
